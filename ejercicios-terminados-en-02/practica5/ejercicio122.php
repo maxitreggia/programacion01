e50d4a2346c45914441a6f0f9777080afbaa5e63
Maxi <maxitreggia@gmail.com>
2023-06-22 19:20:04 -0300

    comienzo practica 5

---
+++ ejercicios-terminados-en-02/practica5/ejercicio122.php
@@ -0,0 +1,21 @@
+<?php
+//Crea una biblioteca de funciones para arrays bidimensionales (de dos dimensiones) de
+//números enteros que contenga las siguientes funciones:
+//1. generaArrayBiInt: Genera un array de tamaño n x m con números aleatorios cuyo
+//intervalo (mínimo y máximo) se indica como parámetro.
+//2. filaDeArrayBiInt: Devuelve la fila i-ésima del array que se pasa como parámetro.
+//3. columnaDeArrayBiInt: Devuelve la columna j-ésima del array que se pasa como
+//parámetro.
+//4. coordenadasEnArrayBiInt: Devuelve la fila y la columna (en un array con dos
+//elementos) de la primera ocurrencia de un número dentro de un array bidimensional.
+//Si el número no se encuentra en el array, la función devuelve el array {-1, -1}.
+//5. esPuntoDeSilla: Dice si un número es o no punto de silla, es decir, mínimo en su fila
+//y máximo en su columna.
+//6. diagonal: Devuelve un array que contiene una de las diagonales del array
+// bidimensional que se pasa como parámetro. Se pasan como parámetros fila, columna
+//y dirección. La fila y la columna determinan el número que marcará las dos posibles
+//diagonales dentro del array. La dirección es una cadena de caracteres que puede ser
+//“deiz” o “izde”. La cadena “deiz” indica que se elige la diagonal que va de derecha a
+//izquierda, mientras que la cadena “izde” indica que se elige la diagonal que va de
+//izquierda a derecha.
+//Probar el funcionamiento de todas.
