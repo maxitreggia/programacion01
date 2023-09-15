cc2d6c455de580097c03aa4ddeaa5264c977a708
Maxi <maxitreggia@gmail.com>
2023-09-15 17:55:53 -0300

    ejercicio 12 terminado

---
+++ ejercicios-terminados-en-02/practica8/ejercicio12.php
@@ -0,0 +1,51 @@
+<?php
+
+use practica8\Circle;
+use practica8\Rectangle;
+use practica8\Square;
+use practica8\Triangle;
+
+require_once 'geometricFigure12.php';
+
+$side1 = floatval($_GET['side1']);
+$side2 = floatval($_GET['side2']);
+$typeOfFigure = $_GET['figure'];
+
+switch ($typeOfFigure) {
+    case 'triangle':
+        if ($side1 > 0 && $side2 > 0) {
+            $triangle = new Triangle('Triángulo');
+            echo "La figura selecionada es: " . $triangle->getName();
+            echo "<br> El área del triángulo es: " . $triangle->calculateArea($side1, $side2);
+        } else {
+            echo "Por favor, ingrese los lados del triángulo. (Lado1 = Base y Lado2 = Altura)";
+        }
+        break;
+    case 'square':
+        if ($side1 > 0) {
+            $square = new Square('Cuadrado');
+            echo "La figura selecionada es: " . $square->getName();
+            echo "<br> EL área del cuadro es: " . $square->calculateArea($side1);
+        } else {
+            echo "Por favor, ingresa el lado del cuadrado. (Lado1 = Lado)";
+        }
+        break;
+    case 'circle':
+        if ($side2 > 0) {
+            $circle = new Circle('Circulo');
+            echo "La figura selecionada es: " . $circle->getName();
+            echo "<br> El área del circulo es: " . $circle->calculateArea($side2);
+        } else {
+            echo "Por favor, ingresa el radio del círculo. (Lado2 = Radio)";
+        }
+        break;
+    case 'rectangle':
+        if ($side1 > 0 && $side2 > 0) {
+            $rectangle = new Rectangle('Rectangulo');
+            echo "La figura selecionada es: " . $rectangle->getName();
+            echo "<br> El área del rectangulo es: " . $rectangle->calculateArea($side1, $side2);
+        } else {
+            echo "Por favor, ingresa los lados del rectángulo. (Lado1 = Base y Lado2 = Altura)";
+        }
+        break;
+}
---
+++ ejercicios-terminados-en-02/practica8/geometricFigure12.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace practica8;
+
+// Clase padre
+class GeometricFigure
+{
+    protected string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+}
+
+// CLases hijas
+
+class Triangle extends geometricFigure
+{
+    public function calculateArea(float $base, float $height):float
+    {
+        return ($base* $height) / 2;
+    }
+}
+
+class Square extends geometricFigure
+{
+    public function calculateArea(float $side):float
+    {
+        return pow($side, 2);
+    }
+}
+
+class Circle extends geometricFigure
+{
+    public function calculateArea(float $radius):float
+    {
+        return pi() * pow($radius, 2);
+    }
+}
+
+class Rectangle extends geometricFigure
+{
+    public function calculaTeArea(float $base, float $height):float
+    {
+        return ($base * $height);
+    }
+}
